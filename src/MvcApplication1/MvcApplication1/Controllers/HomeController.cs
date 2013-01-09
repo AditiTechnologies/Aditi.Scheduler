a1140aedd3e442ae75338a33283870263984ebbe
Wade Wegner <wade.wegner@gmail.com>
2013-01-08 22:50:44 -0700

    Added resources required for generating NuGet packages.

---
+++ src/MvcApplication1/MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Aditi.Scheduler;
+using Aditi.Scheduler.Models;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Uri uri = new Uri("http://devschedule.cloudapp.net/api/task/");
+        private string tenantId = "62022e61-1bd5-4361-bd0d-dff613bbc2dd";
+        private string secretKey = "BWDlg/thd6evmgF256bBJw9HT8KSIqQjAWR/pPqPDUE=";
+
+        public async Task<ActionResult> Index()
+        {
+            var tasks = new Tasks(uri, tenantId, secretKey);
+
+            var o1 = await tasks.GetTasks();
+            ViewBag.Message = o1.Count();
+
+            var task = new TaskModel
+            {
+                Name = "Wade.Test",
+                JobType = JobType.Webhook,
+                CronExpression = "0 0/1 * 1/1 * ? *",
+                Params = new Dictionary<string, object>
+                {
+                    {"url", "http://www.microsoft.com"}
+                }
+            };
+
+            var response = await tasks.CreateTask(task);
+
+            var id = response.Id;
+
+            var o2 = await tasks.GetTasks();
+
+            var o3 = await tasks.DeleteTask(id);
+
+            var o4 = await tasks.GetTasks();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
